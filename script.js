8895f23e1b8a6f3d21b386bc9471e9ea30975043
sungketpatel <93491754+Sungket@users.noreply.github.com>
2024-05-16 14:00:42 +0100

    seperated the concerns of listing out the list from add function

--- script.js
+++ script.js
@@ -3,19 +3,44 @@ const list = [];
 function addToList() {
     const item = document.getElementById("itemText").value;
     list.push(item);
-    // generateList();
-    let shoppingList = document.getElementById("itemList");
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(item));
-    shoppingList.appendChild(li);
+    reGenerateList();
+    // let shoppingList = document.getElementById("itemList");
+    // let li = document.createElement("li");
+    // li.appendChild(document.createTextNode(item));
+    // let deleteBtn = document.createElement("input");
+    // deleteBtn.type = "button";
+    // deleteBtn.value = "Delete";
+    // deleteBtn.addEventListener("click", deleteItem);
+    // deleteBtn.addEventListener("click", deleteItem = (item) => {
+    //     alert("delete!");
+    //     let index = list.indexOf(item);
+    //     list.splice(index, 1);
+    //     reGenerateList();
+    // });
+    // shoppingList.appendChild(li);
+    // shoppingList.appendChild(deleteBtn);
 }
 
-// function generateList() {
-//     for (let i = 0; i < list.length; i++) {
-//         let shoppingList = document.getElementById("itemList");
-//         let li = document.createElement("li");
-//         li.appendChild(document.createTextNode(list[i]));
-//         shoppingList.appendChild(li);
-//     }
-// }
+function reGenerateList() {
+    let shoppingList = document.getElementById("itemList");
+    shoppingList.innerHTML = "";
 
+    for (let i = 0; i < list.length; i++) {
+        // let shoppingList = document.getElementById("itemList");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(list[i]));
+        let deleteBtn = document.createElement("input");
+        deleteBtn.type = "button";
+        deleteBtn.value = "Delete";
+        deleteBtn.addEventListener("click", deleteItem);
+        shoppingList.appendChild(li);
+        shoppingList.appendChild(deleteBtn);
+    }
+}
+
+function deleteItem(item) {
+    alert(item);
+    let index = list.indexOf(item);
+    list.splice(index, 1);
+
+}
