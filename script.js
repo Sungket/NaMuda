89a5f5e2f1cf3017acf95f5c6b80ff20be420809
sungketpatel <93491754+Sungket@users.noreply.github.com>
2024-05-16 09:49:34 +0100

    created form for inputting data and list

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ function add() {
     const item = document.getElementById("itemText");
     list.push(item);
     alert("item added");
+    generateList();
 }
 
+function generateList() {
+    for (let i = 0; i < list.length; i++) {
+        let shoppingList = document.getElementById("itemList");
+        shoppingList.append(list[i]);
+    }
+}
