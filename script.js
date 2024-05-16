acabf8d75ba8ab318b51bfadb30021fb6d55c362
sungketpatel <93491754+Sungket@users.noreply.github.com>
2024-05-16 17:01:10 +0100

    added the sorting functionality for task 2

--- script.js
+++ script.js
@@ -4,21 +4,6 @@ function addToList() {
     const item = document.getElementById("itemText").value;
     list.push(item);
     reGenerateList();
-    // let shoppingList = document.getElementById("itemList");
-    // let li = document.createElement("li");
-    // li.appendChild(document.createTextNode(item));
-    // let deleteBtn = document.createElement("input");
-    // deleteBtn.type = "button";
-    // deleteBtn.value = "Delete";
-    // deleteBtn.addEventListener("click", deleteItem);
-    // deleteBtn.addEventListener("click", deleteItem = (item) => {
-    //     alert("delete!");
-    //     let index = list.indexOf(item);
-    //     list.splice(index, 1);
-    //     reGenerateList();
-    // });
-    // shoppingList.appendChild(li);
-    // shoppingList.appendChild(deleteBtn);
 }
 
 function reGenerateList() {
@@ -26,7 +11,6 @@ function reGenerateList() {
     shoppingList.innerHTML = "";
 
     for (let i = 0; i < list.length; i++) {
-        // let shoppingList = document.getElementById("itemList");
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(list[i]));
         let deleteBtn = document.createElement("input");
@@ -35,14 +19,34 @@ function reGenerateList() {
         deleteBtn.addEventListener("click", function () {
             deleteItem(list[i]);
         }, false );
+        let importantBtn = document.createElement("input");
+        importantBtn.type = "button";
+        importantBtn.value = "Important";
+        importantBtn.id = "important";
+        importantBtn.addEventListener("click", function () {
+            importantize(list[i]);
+        }, false );
         shoppingList.appendChild(li);
         shoppingList.appendChild(deleteBtn);
+        shoppingList.appendChild(importantBtn);
     }
 }
 
 function deleteItem(item) {
-    alert(item);
     let index = list.indexOf(item);
     list.splice(index, 1);
+    reGenerateList();
+}
+
+function importantize(item) {
+    let index = list.indexOf(item);
+    while (index > 0) {
+        let temp = list[index];
+        let itemB = list[index - 1];
+        list[index - 1] = temp;
+        list[index] = itemB;
+        index --;
+    };
+    document.getElementById("important").style.backgroundColor = 'yellow';
     reGenerateList();
 }
