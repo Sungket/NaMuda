1b055a2011d1bb702e086d093f1eb0c8ad96c7be
sungketpatel <93491754+Sungket@users.noreply.github.com>
2024-05-17 09:23:33 +0100

    added the functionality to move the items to the previously bought list

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const list = [];
+const prevList = [];
 
 function addToList() {
     const item = document.getElementById("itemText").value;
@@ -26,9 +27,17 @@ function reGenerateList() {
         importantBtn.addEventListener("click", function () {
             importantize(list[i]);
         }, false );
+        let prevBoughtBtn = document.createElement("input");
+        prevBoughtBtn.type = "button";
+        prevBoughtBtn.value = "prevBought";
+        prevBoughtBtn.id = "previouslyBought";
+        prevBoughtBtn.addEventListener("click", function () {
+            toPrevious(list[i]);
+        }, false );
         shoppingList.appendChild(li);
         shoppingList.appendChild(deleteBtn);
         shoppingList.appendChild(importantBtn);
+        shoppingList.appendChild(prevBoughtBtn);
     }
 }
 
@@ -49,4 +58,44 @@ function importantize(item) {
     };
     document.getElementById("important").style.backgroundColor = 'yellow';
     reGenerateList();
+}
+
+function toPrevious(item) {
+    let index = list.indexOf(item);
+    list.splice(index, 1);
+    prevList.push(item);
+    reGenPrevList();
+    reGenerateList();
+}
+
+function deletePrevItem(item) {
+    let index = prevList.indexOf(item);
+    prevList.splice(index, 1);
+    reGenPrevList();
+}
+
+function reGenPrevList() {
+    let previousList = document.getElementById("previousList");
+    previousList.innerHTML = "";
+
+    for (let i = 0; i < prevList.length; i++) {
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(prevList[i]));
+        let deleteBtn = document.createElement("input");
+        deleteBtn.type = "button";
+        deleteBtn.value = "Delete";
+        deleteBtn.addEventListener("click", function () {
+            deletePrevItem(prevList[i]);
+        }, false );
+        let importantBtn = document.createElement("input");
+        importantBtn.type = "button";
+        importantBtn.value = "Important";
+        importantBtn.id = "important";
+        importantBtn.addEventListener("click", function () {
+            importantize(prevList[i]);
+        }, false );
+        previousList.appendChild(li);
+        previousList.appendChild(deleteBtn);
+        previousList.appendChild(importantBtn);
+    }
 }
